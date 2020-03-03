bddf5ba7e7f876c3e48b68d66f6df7a2f09c5aeb
wlehwjdgh <wlehwjdgh@gmail.com>
2020-03-03 15:26:41 +0900

    #3.17 isLiked Computed File

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,7 +5,6 @@ export default {
   먼저 prisma(db)에서 이 필드를 찾으려고 할거고 
   이 필드는 computed field니까 prisma(db)에 없다.
   그럼 서버에서 찾으려고 할거임
-
   우리가 schema.js파일에서 모든 typeDefs와 resolvers를 통합했기 때문에
   모든 query, mutation에서 동작 가능
 */
@@ -43,6 +42,30 @@ export default {
       const {id: parentId } = parent;
       return user.id === parentId;
     }
-  }
+	},
+	Post: {
+		isLiked: (parent, _, { request }) => {
+			const { id } = parent;
+			const { user } = request;
 
+			try{
+				return prisma.$exists.like({
+					AND: [
+						{
+							user: {
+								id: user.id
+							}
+						},
+						{
+							post: {
+								id
+						}}
+					]
+				});
+			}catch(error){
+				return false;
+			}
+			
+		}
+	}
 }

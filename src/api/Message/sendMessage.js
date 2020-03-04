26acd9c3e28330f414cb5b603e5e96fd0f00db0e
김정호 <wlehwjdgh@gmail.com>
2020-03-04 23:26:13 +0900

    #3.24 sendMessage part Two

--- src/api/Message/sendMessage.js
+++ src/api/Message/sendMessage.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../fragments";
 
 export default {
 	Mutation: {
@@ -10,21 +11,40 @@ export default {
 
 			if(roomId === undefined){
 				if(toId !== user.id){
-					const room = await prisma.createRoom({
+					room = await prisma.createRoom({
 						participants:{
 							connect:[{id:toId},{id:user.id}]
 						}
-					});
-				}else{
-					room = await prisma.room({id: roomId});
+					}).$fragment(ROOM_FRAGMENT);
 				}
-				if(!room){
-					throw Error("Room not found");
-				}
-
-				//not complete yet
-				return null;
+			}else{
+				room = await prisma.room({id: roomId}).$fragment(ROOM_FRAGMENT);
 			}
+			
+			if(!room){
+				throw Error("Room not found");
+			}
+			
+			const getTo = room.participants.filter(
+				participant => participant.id !== user.id
+			)[0];
+
+			return prisma.createMessage({
+				text: message,
+				from:{
+					connect:{id:user.id}
+				},
+				to:{
+					connect:{
+						id: roomId? getTo.id : toId
+					}
+				},
+				room:{
+					connect:{
+						id:room.id
+					}
+				}
+			});
 		}
 	}
-}
+};
--- src/fragments.js
+++ src/fragments.js
@@ -32,3 +32,10 @@ export const FULL_POST_FRAGMENT = `
 		}
 	}
 `;
+
+export const ROOM_FRAGMENT = `
+	fragment RoomParts on Room {
+		id
+		participants {id}
+	}
+`;

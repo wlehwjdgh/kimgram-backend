6328a32008a8d6493fa51f89c0cd8d12e50a72eb
김정호 <wlehwjdgh@gmail.com>
2020-03-11 07:55:02 +0900

    refactoring createAccount username check

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -5,6 +5,10 @@ export default {
     Mutation: {
         createAccount: async (_, args) => {
             const { username, email, firstName = "", lastName = "", bio = "" } = args;
+            const exists = await prisma.$exists.user({username});
+            if(exists) {
+                throw Error("This username is already taken");
+            }
             try{
                 await prisma.createUser({
                     username,

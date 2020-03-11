7ef1f3aabb5103cb79d75d39d75c2679790d9047
wlehwjdgh <wlehwjdgh@gmail.com>
2020-03-11 15:57:28 +0900

    refactor createAccount mutation - check username/email duplication

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,25 +2,31 @@ import { prisma } from "../../../../generated/prisma-client";
 
 
 export default {
-    Mutation: {
-        createAccount: async (_, args) => {
-            const { username, email, firstName = "", lastName = "", bio = "" } = args;
-            const exists = await prisma.$exists.user({username});
-            if(exists) {
-                throw Error("This username is already taken");
-            }
-            try{
-                await prisma.createUser({
-                    username,
-                    email,
-                    firstName,
-                    lastName,
-                    bio
-                 });
-                 return true;
-            }catch{
-                return false;
-            }
-        }
-    }
+	Mutation: {
+		createAccount: async (_, args) => {
+			const { username, email, firstName = "", lastName = "", bio = "" } = args;
+			const exists = await prisma.$exists.user({
+				OR:[
+					{ username },
+					{ email	}
+				]
+			});
+
+			if(exists) {
+				throw Error("This username/ email is already taken");
+			}
+			try{
+			await prisma.createUser({
+					username,
+					email,
+					firstName,
+					lastName,
+					bio
+				});
+				return true;
+			}catch{
+				return false;
+			}
+		}
+	}
 };
